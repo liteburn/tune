e91409cb0f22037effa4be4daf5b57d3f3d0b9d5
Arsen <43439005+liteburn@users.noreply.github.com>
2020-04-09 16:49:45 +0300

    Update tune.js
    Some small changes.

--- tune.js
+++ tune.js
@@ -1,37 +1,29 @@
 function tune(arr) {
     // Your code goes here
+    function res(dif){
+        if (arr[i] === 0){
+            new_arr.push("-");
+        } else if (dif === 0){
+                new_arr.push("OK");
+        } else if (dif < 0){
+            if (dif <= -0.03){
+                new_arr.push(">>•");
+            }
+            else{
+                new_arr.push(">•");
+            }
+        } else if (dif >= 0.03){
+                new_arr.push("•<<");
+        }else{
+            new_arr.push("•<");
+        }
+    }
     let dif;
     const lst = [329.63, 246.94, 196.00, 146.83, 110.00, 82.41];
     let new_arr = [];
     for (i = 0; i < 6; i++){
         dif = (arr[i] - lst[i])/lst[i];
-        if (arr[i] === 0){
-            new_arr.push("-");
-        }
-        else{
-            if (dif === 0){
-                new_arr.push("OK");
-            }
-            else{
-                if (dif < 0){
-                    if (dif <= -0.03){
-                        new_arr.push(">>•");
-                    }
-                    else{
-                        new_arr.push(">•");
-                    }
-                }
-                else{
-                    if (dif >= 0.03){
-                        new_arr.push("•<<");
-                    }
-                    else{
-                        new_arr.push("•<");
-                    }
-                }
-            }
-        }
-
+        res(dif);
     }
     return new_arr;
 }
